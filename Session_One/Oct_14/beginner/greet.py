a98ae50aceb4734a754e88d145f879085f178317
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-10-14 11:12:38 -0700

    Update greet.py

--- Session_One/Oct_14/beginner/greet.py
+++ Session_One/Oct_14/beginner/greet.py
@@ -1,2 +1,4 @@
 name = input('Hello! What is your name?\n')
 print('nice to meet you, %s.' % name)
+pokemon = input('What is your favorite Pokemon?\n')
+print(name,'\'s favorite Pokemon is', pokemon)
