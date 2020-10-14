5d2b09991b9f87d2f65cbf5ab5fe20adb0f9e4f2
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-10-14 11:01:57 -0700

    Update greet.cpp

--- Session_One/Oct_14/beginner/greet.cpp
+++ Session_One/Oct_14/beginner/greet.cpp
@@ -3,8 +3,12 @@
 int main() {
 
   std::string name;
+  std::string pokemon;
   std::cout << "Hello! What's your name?\n";
   std::cin >> name;
-  std::cout << "Nice to meet you, " << name;
+  std::cout << "Nice to meet you, " << name << "\n";
+  std::cout << "What is your favorite Pokemon?\n";
+  std::cin >> pokemon;
+  std::cout << name<< "\'s favorite Pokemon is " << pokemon;
   return 0;
 }
