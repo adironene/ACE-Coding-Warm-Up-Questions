0002e6e546284e672d6e74060325195a72f35f37
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-10-14 10:44:03 -0700

    Create greet.cpp

---
+++ Session_One/Oct_14/beginner/greet.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+  std::string name;
+  std::cout << "Hello! What's your name?\n";
+  std::cin >> name;
+  std::cout << "Nice to meet you, " << name;
+  return 0;
+}
