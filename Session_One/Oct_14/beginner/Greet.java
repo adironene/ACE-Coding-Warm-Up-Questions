800915152ce6438ee73dbefc8fa774e623bbeece
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-10-14 10:51:22 -0700

    Update Greet.java

--- Session_One/Oct_14/beginner/Greet.java
+++ Session_One/Oct_14/beginner/Greet.java
@@ -5,6 +5,9 @@ class Greet {
        Scanner writer = new Scanner(System.in);  
        System.out.println("Hello! what is your name?"); 
        String name = writer.nextLine(); 
-       System.out.println("nice to meet you " + name + "!"); 
+       System.out.println("nice to meet you " + name + "."); 
+       System.out.println("What is your favorite Pokemon?"); 
+       String pokemon = writer.nextLine(); 
+       System.out.println(name + "\'s favorite Pokemon is " + pokemon + "."); 
     }
 }
